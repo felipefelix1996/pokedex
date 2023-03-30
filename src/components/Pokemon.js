c49709de47474bd8e2369eb08cd7bce535d87284
Felipe Felix <felipe.felix@acct.global>
2023-03-30 18:37:18 -0300

    styles[component]: card style

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,9 +2,44 @@ import React from "react";
 
 const Pokemon = (props) => {
   const { pokemon } = props;
+  const onHeartClick = () => {
+    console.log("pode favoritar", pokemon);
+  };
+  const heart = "❤️";
   return (
-    <div>
-      <div>Nome: {pokemon.name}</div>
+    <div className="pokemon-card">
+      <div className="pokemon-card-top">
+        <div className="pokemon-card-image">
+          <img
+            alt={pokemon.name}
+            src={pokemon.sprites.front_default}
+            className="pokemon-image"
+          />
+        </div>
+        <div className="pokemon-info-name">
+          <h3>{pokemon.name}</h3>
+          <div>{pokemon.id}</div>
+        </div>
+      </div>
+      <div className="pokemon-card-bottom">
+        <div className="pokemon-type">
+          {pokemon.types.map((type, index) => {
+            return (
+              <div
+                key={index}
+                className={`${
+                  type.type.name ? type.type.name : ""
+                } pokemon-type-text`}
+              >
+                {type.type.name}
+              </div>
+            );
+          })}
+        </div>
+        <button className="pokemon-heart-byn" onClick={onHeartClick}>
+          {heart}
+        </button>
+      </div>
     </div>
   );
 };
